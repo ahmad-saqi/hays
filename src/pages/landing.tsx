032c20bf4c8f8388e1b86376f3ee63a0cec78f4f
ahmad-saqi <ahmad_saqi@digimarkdevelopers.com>
2025-07-30 10:48:12 +0500

    add

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,41 +1,11 @@
-import Ambition from "@/components/ambition";
-import Career from "@/components/career";
-import CareerAdvice from "@/components/career-advice";
-import CareerHub from "@/components/career-hub";
-import CTA from "@/components/cta";
-import Featured from "@/components/featured";
-import Footer from "@/components/footer";
 import Hero from "@/components/hero";
-import LookingToHire from "@/components/looking-to-hire";
 import Navabr from "@/components/navbar";
-import Recruitment from "@/components/recruitment";
 
 const Landing = () => {
   return (
     <div>
       <Navabr />
       <Hero />
-      <Career />
-      <div className="bg-blue-950">
-        <LookingToHire />
-      </div>
-      <CareerAdvice />
-      <div className="bg-yellow-100">
-        <Recruitment />
-      </div>
-      <Ambition />
-      <div className="bg-yellow-100">
-        <Featured />
-      </div>
-      <div className="bg-blue-300">
-        <CareerHub />
-      </div>
-      <div className="bg-yellow-100">
-        <CTA />
-      </div>
-      <div className="bg-blue-950">
-        <Footer />
-      </div>
     </div>
   );
 };
